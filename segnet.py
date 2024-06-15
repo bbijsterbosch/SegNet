7bed3261a1a7d108b3a9a123afd00ab58fc51369
Bas Bijsterbosch <b.a.bijsterbosch@student.tudelft.nl>
2024-06-15 15:36:40 +0200

    added metrics but they dont work correctly yet

--- segnet.py
+++ segnet.py
@@ -8,7 +8,7 @@ from itertools import product
 import torch.nn.functional as F
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
-
+from torch.utils.checkpoint import checkpoint
 class SegNet(nn.Module):
     def __init__(self, input_channels, initial_output_channels, num_classes, kernel_size, pool_kernel_size):
         super(SegNet, self).__init__()
@@ -19,7 +19,7 @@ class SegNet(nn.Module):
         self.decoder_layers = nn.ModuleList()
         self.vgg16 = models.vgg16(pretrained=True)
         output_channels = initial_output_channels
-
+        
         # Encoder: First 2 blocks with 2 convolutions each
         for _ in range(2):
             self.encoder_layers.append(nn.Conv2d(input_channels, output_channels, kernel_size, padding=1))
@@ -108,6 +108,7 @@ class SegNet(nn.Module):
                 x = self.encoder_layers[layer_idx](x)
                 x = F.relu(x)
                 layer_idx += 1
+               
             sizes.append(x.size())
             x, indices = self.pooling_layers[i](x)
             pool_indices.append(indices)
@@ -132,14 +133,14 @@ class SegNet(nn.Module):
             x = self.unpooling_layers[i](x, pool_indices[-(i+1)], output_size=sizes[-(i+1)])
             if i == 3:  # Last 2 blocks with 2 convolutions each
                 for _ in range(2):
-                    x = self.decoder_layers[layer_idx](x)
+                    x = checkpoint(self.decoder_layers[layer_idx], x)
                     x = F.relu(x)
                     layer_idx += 1
                     x = self.decoder_layers[layer_idx](x)
                     x = F.relu(x)
                     layer_idx += 1
             elif i == 4:
-                x = self.decoder_layers[layer_idx](x)
+                x = checkpoint(self.decoder_layers[layer_idx],x)
                 x = F.relu(x)
                 layer_idx += 1
                 x = self.decoder_layers[layer_idx](x)
@@ -148,7 +149,7 @@ class SegNet(nn.Module):
                 x = self.decoder_layers[layer_idx](x)              
             else:  # First 3 blocks with 3 convolutions each
                 for _ in range(3):
-                    x = self.decoder_layers[layer_idx](x)
+                    x = checkpoint(self.decoder_layers[layer_idx],x)
                     x = F.relu(x)
                     layer_idx += 1
                     x = self.decoder_layers[layer_idx](x)
@@ -179,16 +180,42 @@ class SegNet(nn.Module):
             
 
 class Train_Test:
-    def __init__(self, model, device, trainloader, lr, momentum, epochs):
+    def __init__(self, model, device, trainloader, testloader, classes, lr, momentum, epochs):
         
         self.lr = lr
         self.momentum = momentum
         self.epochs = epochs
         self.trainloader = trainloader
+        self.testloader = testloader
         self.device = device
         self.model = model
+        self.classes = classes
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=self.momentum)
         self.loss_fn = nn.CrossEntropyLoss().to(self.device)
+        
+    def compute_miou(pred, target, num_classes):
+        #Compute the Intersection over Union (IoU) for each class.
+        ious = []
+        pred = pred.view(-1)
+        target = target.view(-1)
+
+        for cls in range(num_classes):
+            pred_inds = pred == cls
+            target_inds = target == cls
+            intersection = (pred_inds[target_inds]).sum().item()
+            union = pred_inds.sum().item() + target_inds.sum().item() - intersection
+            if union == 0:
+                ious.append(float('nan'))  # If there is no ground truth, do not include in evaluation
+            else:
+                ious.append(float(intersection) / max(union, 1))
+        
+        ious = np.arrary(ious)
+        mean_iou = np.nanmean(ious)
+
+        return mean_iou
+    
+
+
     def train(self):
         #Set the optimizer and loss function
         self.model.to(self.device)
@@ -197,10 +224,11 @@ class Train_Test:
         prev_loss = float('inf')
         run_epoch = self.epochs
         writer = SummaryWriter(log_dir='runs/experiment1')
-        for epoch in tqdm(range(1, run_epoch + 1)):
+        
+        for epoch in range(1, run_epoch + 1):
             sum_loss = 0.0
 
-            for j, data in enumerate(self.trainloader, 1):
+            for j, data in tqdm(enumerate(self.trainloader, 1), total=len(self.trainloader)):
                 images, labels = data
                 images = images.to(self.device, dtype=torch.float32)
                 labels = labels.to(self.device)
@@ -211,17 +239,66 @@ class Train_Test:
                 self.optimizer.step()
 
                 sum_loss += loss.item()
-                torch.cuda.empty_cache()
-                print('Loss at {} mini-batch: {}'.format(j, loss.item()/self.trainloader.batch_size))
+        
+                
             loss_epoch = sum_loss/j*self.trainloader.batch_size
-            writer.add_scalar('Loss', loss_epoch)
+            writer.add_scalar('Loss/Train', loss_epoch)
             print('Average loss @ epoch {}: {}'.format(epoch, loss_epoch))
+
+            #Evaluate the test set
+            self.model.eval()
+            test_loss = 0.0
+            correct_pixels = 0
+            total_pixels = 0
+            iou_scores = []
+            
+            class_correct = np.zeros(len(self.classes))
+            class_total = np.zeros(len(self.classes))
+
+            with torch.no_grad():
+                for data in self.testloader:
+                    images, labels = data
+                    images = images.to(self.device, dtype=torch.float32)
+                    labels = labels.to(self.device)
+                    output_softmax = self.model(images)
+                    print(output_softmax.size())
+                    loss = self.loss_fn(output_softmax, labels)
+                    test_loss += loss.item()
+                    _, predicted = torch.max(output_softmax, 1)
+                    
+                    predicted = predicted.view(-1)
+                    labels = labels.view(-1)
+                    total_pixels += labels.size(0)
+                    correct_pixels += (predicted == labels).sum().item()
+                    
+                    for cls in range(self.classes):
+                        class_total[cls] += (labels == cls).sum().item()
+                        class_correct[cls] += ((predicted == cls) & (labels == cls)).sum().item()
+                        
+                    pred = predicted.cpu().numpy()
+                    label = labels.cpu().numpy()
+                    iou_scores.append(self.compute_miou(pred, label, self.num_classes))
+
+            test_loss /= len(self.testloader)
+            acc = correct / total
+            class_acc = class_correct / class_total
+            class_avg_acc = np.mean(class_acc)
+            avg_miou = np.mean(iou_scores)
+            writer.add_scalar('Loss/Test', test_loss, epoch)
+            writer.add_scalar('Global Accuracy', acc, epoch)
+            writer.add_scalar('Class Avg. Accuracy', class_avg_acc, epoch)
+            writer.add_scalar('mIoU', avg_miou, epoch)
+
             is_better = loss_epoch < prev_loss
             if is_better:
                 prev_loss = loss_epoch
                 torch.save(self.model.state_dict(), "model_best.pth")
+
+
+        self.model.train()
         torch.cuda.empty_cache()
-    def test(self):
+
+    def eval(self):
         self.model.to(self.device)
         self.model.eval()
         classes = np.load('/home/bas/Robotics/CV/SegNet/CamVidDataSet/CamVid/seg_classes.npy')
@@ -242,61 +319,28 @@ class Train_Test:
                     rgb_image[i, j] = classes[class_idx]
             plt.imshow(rgb_image)
             plt.show()
-        # # Check if the number of classes in the one-hot encoded image matches the number of RGB values
-        # assert num_classes == 32, "Number of classes in one-hot encoded image should match number of RGB values"
-
-        # # Create an empty array for the RGB image
-        # rgb_image = np.zeros((height, width, 3), dtype=np.uint8)
-
-        # # Iterate over each pixel and map the one-hot encoded value to the RGB value
-        # for i in range(height):
-        #     for j in range(width):
-        #         # Find the class index (the position where the value is 1)
-        #         class_idx = np.argmax(label[i, j])
-        #         # Get the RGB value for the class index
-        #         rgb_image[i, j] = classes[class_idx]
-        
-        # color_map = torch.tensor(list(product([63, 127, 191, 255], repeat=3)))
-
-        # # print()
-        # # print("Map of class to color: ")
-        # # for class_ind, color in enumerate(color_map):
-        # #     print("Class: {}, RGB Color: {}".format(class_ind + 1, color))
-
-        
-        # for i, data in enumerate(self.trainloader):
-        #     images = data[0].to(self.device, dtype=torch.float32)
-        #     res = self.model(images)
-        #     res = torch.argmax(res, dim=1) # one-hot squashed to pixel-wise labels
-
-        #     for n in range(res.shape[0]): # loop over each image
-        #         res_image = color_map[res[n].cpu()].permute(2, 0, 1).to(torch.float).div(255.0) # transpose back to C, H, W, normalize to (0.0, 1.0)
-        #         save_image(res_image, os.path.join('outputs', "img_{}_{}.png".format(i, n)))
-
-    #def test(self):
+  
    
-      
-
-
+    
 # Example usage
 # input_channels = 3
 # initial_output_channels = 64
 # kernel_size = 3
 # pool_kernel_size = 2
 # num_classes = 32
-# device = F.device("cuda" if F.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # model = SegNet(input_channels, initial_output_channels,num_classes, kernel_size, pool_kernel_size)
-# model.init_vgg16_weigths()
-# for layer in model.decoder_layers:
-#     print(layer)
-# model.to(device)
-# # layers = 0
-# # for layer in model.encoder_layers:
+# # model.init_vgg16_weigths()
+# # for layer in model.decoder_layers:
 # #     print(layer)
-# #     layers += 1
+# model.to(device)
+# # # layers = 0
+# # # for layer in model.encoder_layers:
+# # #     print(layer)
+# # #     layers += 1
 
-# # x = F.randn(1, 3, 32, 32)
-# # output = model(x)
+# x = torch.randn(1, 3, 32, 32).to(device)
+# # # output = model(x)
 # summary(model, (3,32,32))
-# print(output.size())
+# # print(output.size())
--- train.py
+++ train.py
@@ -15,7 +15,7 @@ os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 def main():
     #Load in the file where the classes for each pixel are specified
     #Note: these paths are relative
-    train = False
+    train = True
 
 
     input_channels = 3
@@ -27,111 +27,32 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     summary(model.to(device), (3,32,32))
     if train:
+
         model.init_vgg16_weigths()
         path_classes      = "CamVidDataSet/CamVid/seg_classes.npy"
-        #Get the directory where the training images are stored
+
+        #Get the directory where the training and test images are stored
         path_no_lbl_train = "CamVidDataSet/CamVid/train"
         path_lbl_train   = "CamVidDataSet/CamVid/train_labels"
+        path_no_lbl_test = "CamVidDataSet/CamVid/test"
+        path_lbl_test = "CamVidDataSet/CamVid/test_labels"
         classes = np.load(path_classes)
-
         #Dont forget this line need to transform our dataset into somthing pytorch can use.
         transform = transforms.Compose([transforms.ToTensor()])
 
         trainset = CamVidLoader(classes, path_no_lbl_train,  path_lbl_train,  transform=transform)
-        
+        testset = CamVidLoader(classes, path_no_lbl_test, path_lbl_test, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=7, shuffle=True, num_workers=4)
-        trainer_tester = segnet.Train_Test(model, device, trainloader, lr=0.1, momentum=0.9, epochs=500)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=7, shuffle = True, num_workers=4)
+        trainer_tester = segnet.Train_Test(model, device, trainloader,testloader, classes,  lr=0.1, momentum=0.9, epochs=500) 
         
         trainer_tester.train()
-        # train_features, train_labels = next(iter(trainloader))
-        # print(f"Feature batch shape: {train_features.size()}")
-        # print(f"Labels batch shape: {train_labels.size()}")
-        # img = train_features[0].squeeze()
-        # img = torch.transpose(img, 0, 2)
-        # label = train_labels[0].squeeze()
-        # label = torch.transpose(label, 0, 2)
-        # print(label.shape)
-
-        # # classes = np.load("/home/jzwanen/computer_vision/SegNet/CamVidDataSet/CamVid/seg_classes.npy")
-        # # print(classes)
-        # # print(type(classes))
-        # # print(np.shape(classes))
-
-
-        # # Ensure the RGB values have the correct shape (32, 3)
-        # assert classes.shape == (32, 3), "RGB values should have shape (32, 3)"
-
-        # # Get the shape of the one-hot encoded image
-        # height, width, num_classes = label.shape
-
-        # # Check if the number of classes in the one-hot encoded image matches the number of RGB values
-        # assert num_classes == 32, "Number of classes in one-hot encoded image should match number of RGB values"
-
-        # # Create an empty array for the RGB image
-        # rgb_image = np.zeros((height, width, 3), dtype=np.uint8)
-
-        # # Iterate over each pixel and map the one-hot encoded value to the RGB value
-        # for i in range(height):
-        #     for j in range(width):
-        #         # Find the class index (the position where the value is 1)
-        #         class_idx = np.argmax(label[i, j])
-        #         # Get the RGB value for the class index
-        #         rgb_image[i, j] = classes[class_idx]
-
-
-        # plt.imshow(rgb_image)
-        # plt.show()
-
-        # model = model.to(device)
-        # trainer_tester.train()
-        # train_features, train_labels = next(iter(trainloader))
-        # print(f"Feature batch shape: {train_features.size()}")
-        # print(f"Labels batch shape: {train_labels.size()}")
-        # img = train_features[0].squeeze()
-        # img = torch.transpose(img, 0, 2)
-        # label = train_labels[0].squeeze()
-        # label = torch.transpose(label, 0, 2)
-        # print(label.shape)
-
-
-
-
-
-
-        # # classes = np.load("/home/jzwanen/computer_vision/SegNet/CamVidDataSet/CamVid/seg_classes.npy")
-        # # print(classes)
-        # # print(type(classes))
-        # # print(np.shape(classes))
-
-
-        # # Ensure the RGB values have the correct shape (32, 3)
-        # assert classes.shape == (32, 3), "RGB values should have shape (32, 3)"
-
-        # # Get the shape of the one-hot encoded image
-        # height, width, num_classes = label.shape
-
-        # # Check if the number of classes in the one-hot encoded image matches the number of RGB values
-        # assert num_classes == 32, "Number of classes in one-hot encoded image should match number of RGB values"
-
-        # # Create an empty array for the RGB image
-        # rgb_image = np.zeros((height, width, 3), dtype=np.uint8)
-
-        # # Iterate over each pixel and map the one-hot encoded value to the RGB value
-        # for i in range(height):
-        #     for j in range(width):
-        #         # Find the class index (the position where the value is 1)
-        #         class_idx = np.argmax(label[i, j])
-        #         # Get the RGB value for the class index
-        #         rgb_image[i, j] = classes[class_idx]
-
-
-        # plt.imshow(rgb_image)
-        # plt.show()
+       
     else:
                 
         path_classes = "CamVidDataSet/CamVid/seg_classes.npy"
-        path_no_lbl_test = "CamVidDataSet/CamVid/test"
-        path_lbl_test   = "CamVidDataSet/CamVid/test"
+        path_no_lbl_test = "CamVidDataSet/CamVid/val"
+        path_lbl_test   = "CamVidDataSet/CamVid/val_labels"
         transform = transforms.Compose([transforms.ToTensor()])
         classes = np.load(path_classes)
         test_set = CamVidLoader(classes, path_no_lbl_test, path_lbl_test, transform=transform)
