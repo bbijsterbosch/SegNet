b08d656b51a8a74b34c13f6b566ad4def500eb05
wleijendekker <wleijendekker@tudelft.nl>
2024-06-16 10:20:18 +0200

    Made an argument parser for train function

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from DataLoader.CamVidDataLoader import CamVidLoader
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import argparse
+
 
 
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
@@ -15,14 +17,25 @@ os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 def main():
     #Load in the file where the classes for each pixel are specified
     #Note: these paths are relative
-    train = True
+    parser = argparse.ArgumentParser('Train/testing script')
+
+    parser.add_argument('train', type=bool, help='Set to True for training, False for testing')
+    parser.add_argument('--input_channels', type=int, help='Set amount of input channels to int', default=3)
+    parser.add_argument('--initial_output_channels', type=int, default=64)
+    parser.add_argument('--kernel_size', type=int, default=3)
+    parser.add_argument('--pool_kernel_size', type=int, default=2)
+    parser.add_argument('--num_classes', type=int, default=32)
 
 
-    input_channels = 3
-    initial_output_channels = 64
-    kernel_size = 3
-    pool_kernel_size = 2
-    num_classes = 32
+
+    args = parser.parse_args()
+
+    train = args.train
+    input_channels = args.input_channels
+    initial_output_channels = args.initial_output_channels
+    kernel_size = args.kernel_size
+    pool_kernel_size = args.pool_kernel_size
+    num_classes = args.num_classes
 
     model = segnet.SegNet(input_channels, initial_output_channels, num_classes,kernel_size, pool_kernel_size)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -43,8 +56,8 @@ def main():
 
         trainset = CamVidLoader(classes, path_no_lbl_train,  path_lbl_train,  transform=transform)
         valset = CamVidLoader(classes, path_no_lbl_test, path_lbl_test, transform=transform)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=7, shuffle=True, num_workers=4)
-        valloader = torch.utils.data.DataLoader(valset, batch_size=7, shuffle = True, num_workers=4)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=2, shuffle=True, num_workers=4)
+        valloader = torch.utils.data.DataLoader(valset, batch_size=2, shuffle = True, num_workers=4)
         trainer_tester = segnet.Train_Test(model, device, trainloader, valloader, classes,  lr=0.1, momentum=0.9, epochs=800) 
         
         trainer_tester.train()
